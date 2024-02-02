512b3bc2ff84647acc849a23a3b810f7558f13aa
bpawlowski <bartosz.pawlowski@decerto.com>
2024-02-02 11:29:26 +0100

    fix: invalid expectations when 401 status code is returned

--- src/test/groovy/pl/decerto/rekrutacja/bpawlowski/HelloResourceV1IntegrationTest.groovy
+++ src/test/groovy/pl/decerto/rekrutacja/bpawlowski/HelloResourceV1IntegrationTest.groovy
@@ -23,15 +23,11 @@ class HelloResourceV1IntegrationTest extends ResourceIntegrationTestUtils {
     def "/api/v1/hello/secured - it should reject unauthenticated request"() {
         given: 'unauthenticated request'
             def greetedPerson = "decerto-candidate"
-            def expectedGreet = "Hello, ${greetedPerson}"
             def request = unauthenticated()
         when: 'hello endpoint is called'
             def response = request.get("${RESOURCE_URL}/secured/${greetedPerson}")
         then: 'response status should be 401 unauthorized'
             401 == response.statusCode()
-        then: 'response body should contain greet message'
-            def responseBody = response.as(new TypeRef<ValueWrapper<String>>(){})
-            expectedGreet == responseBody.value
     }
 
     def "/api/v1/hello/secured - it should greet me"() {
